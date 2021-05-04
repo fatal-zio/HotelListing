456f80341aa1ecfa91e24bdb3adcad45f137f21d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2021-05-03 21:34:48 -0500

    Added GetAll() functionality.

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                return  Ok(_mapper.Map<IList<CountryDto>>(countries));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error retrieving countries. Method: {nameof(GetCountries)}. Message: {ex.Message}");
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+    }
+}
