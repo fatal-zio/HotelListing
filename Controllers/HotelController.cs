a25f4abda29e87392ce9edcd7005d09785c53be5
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2021-05-05 21:16:46 -0500

    Implemented GET functionality.

--- Controllers/HotelController.cs
+++ Controllers/HotelController.cs
@@ -12,13 +12,13 @@ namespace HotelListing.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class CountryController : ControllerBase
+    public class HotelController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly ILogger<CountryController> _logger;
+        private readonly ILogger<HotelController> _logger;
         private readonly IMapper _mapper;
 
-        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
             _logger = logger;
@@ -28,16 +28,16 @@ namespace HotelListing.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetCountries()
+        public async Task<IActionResult> GetHotels()
         {
             try
             {
-                var countries = await _unitOfWork.Countries.GetAll();
-                return Ok(_mapper.Map<IList<CountryDto>>(countries));
+                var countries = await _unitOfWork.Hotels.GetAll();
+                return Ok(_mapper.Map<IList<HotelDto>>(countries));
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error retrieving countries. Method: {nameof(GetCountries)}. Message: {ex.Message}");
+                _logger.LogError(ex, $"Error retrieving countries. Method: {nameof(GetHotels)}. Message: {ex.Message}");
                 return StatusCode(500, "Internal Server Error.");
             }
         }
@@ -45,16 +45,16 @@ namespace HotelListing.Controllers
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetCountry(int id)
+        public async Task<IActionResult> GetHotel(int id)
         {
             try
             {
-                var country = await _unitOfWork.Countries.Get(o => o.Id == id, new List<string> { "Hotels" });
-                return Ok(_mapper.Map<CountryDto>(country));
+                var hotel = await _unitOfWork.Hotels.Get(o => o.Id == id, new List<string> { "Country" });
+                return Ok(_mapper.Map<HotelDto>(hotel));
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, $"Error retrieving countries. Method: {nameof(GetCountry)}. Message: {ex.Message}");
+                _logger.LogError(ex, $"Error retrieving countries. Method: {nameof(GetHotel)}. Message: {ex.Message}");
                 return StatusCode(500, "Internal Server Error.");
             }
         }
